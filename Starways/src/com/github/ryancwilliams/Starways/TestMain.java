c082bf544c42b98e3f40cffd7f603c184cd7b701
Ryan Williams <rcw4rcw@gmail.com>
2013-06-29 17:57:54 -0400

    Created a Testing Main Class
    Created a class to use for testing new classes.

---
+++ Starways/src/com/github/ryancwilliams/Starways/TestMain.java
@@ -0,0 +1,15 @@
+package com.github.ryancwilliams.Starways;
+
+/**
+ * This is a main class for testing the application
+ * @author ryancwilliams
+ */
+public class TestMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
