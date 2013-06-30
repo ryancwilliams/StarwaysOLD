ebbe1175ba3b2e3c1d4341c3523190442a4b2c10
Ryan Williams <rcw4rcw@gmail.com>
2013-06-30 15:11:50 -0400

    Fixed typo in package-info for the ValuesOfQuantities package.

--- Starways/src/com/github/ryancwilliams/Starways/engine/math/ValuesOfQuantities/package-info.java
+++ Starways/src/com/github/ryancwilliams/Starways/engine/math/ValuesOfQuantities/package-info.java
@@ -7,6 +7,6 @@
  * Provides the classes for working with Values of Quantities.
  * A Value of Quantity is a numerical value that is in a specified unit, or 
  * quantity. These classes makes it easer to work with Values of Quantities 
- * and should be used when possible
+ * and should be used when possible.
  */
 package com.github.ryancwilliams.Starways.engine.math.ValuesOfQuantities;
