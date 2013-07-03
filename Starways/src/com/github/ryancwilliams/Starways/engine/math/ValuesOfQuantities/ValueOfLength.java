dda33aff7e58abfed2bc838bea5a0f12861777db
Ryan Williams <rcw4rcw@gmail.com>
2013-07-03 01:14:50 -0400

    Changed ValueOfQuantity to a abstract class
    This was done to make it easer to add additional quantity systems.
    Contributes to #9

--- Starways/src/com/github/ryancwilliams/Starways/engine/math/ValuesOfQuantities/ValueOfLength.java
+++ Starways/src/com/github/ryancwilliams/Starways/engine/math/ValuesOfQuantities/ValueOfLength.java
@@ -8,12 +8,8 @@ package com.github.ryancwilliams.Starways.engine.math.ValuesOfQuantities;
  * Used to link numerical values of length to Quantities of Length
  * @author ryancwilliams
  */
-public class ValueOfLength implements ValueOfQuantity {
+public class ValueOfLength extends ValueOfQuantity {
     
-    /**
-     * the value in the specified quantity.
-     */
-    private double value;
     /**
      * the quantity the value is specified in.
      */
@@ -27,23 +23,6 @@ public class ValueOfLength implements ValueOfQuantity {
     public ValueOfLength(double value, QuantityOfLength quantity) {
         this.value = value;
         this.quantity = quantity;
-    }
-
-    /**
-     * Gets the value in the specified quantity.
-     * @return the value in the specified quantity.
-     */
-    @Override
-    public double getValue() {
-        return value;
-    }
-    
-    /**
-     * Sets the value in the specified quantity.
-     * @param value the value to set specified quantity into.
-     */
-    public void setValue(double value) {
-        this.value = value;
     }
     
     /**
@@ -89,17 +68,6 @@ public class ValueOfLength implements ValueOfQuantity {
     public void convertTo(QuantityOfLength quantity) {
         this.value = this.getConvertedValue(quantity);
         this.quantity = quantity;
-    }
-
-    /**
-     * Gets this ValueOfLength as a formated string.
-     * @param precision the number decimals to create the value to.
-     * @return the ValueOfQuantity as a formated string.
-     */
-    @Override
-    public String asString(int precision) {
-        //Call the formator to format this value.
-        return ValueOfQuantityFormatter.formatValueOfQuantity(this, precision);
     }
 
     /**
--- Starways/src/com/github/ryancwilliams/Starways/engine/math/ValuesOfQuantities/ValueOfQuantity.java
+++ Starways/src/com/github/ryancwilliams/Starways/engine/math/ValuesOfQuantities/ValueOfQuantity.java
@@ -6,34 +6,58 @@ package com.github.ryancwilliams.Starways.engine.math.ValuesOfQuantities;
 
 /**
  * An interface for reading values inside of a ValueOfQuantity object.
+ *
  * @author ryancwilliams
  */
-public interface ValueOfQuantity {
-    
+public abstract class ValueOfQuantity {
+
+    /**
+     * the value in the specified quantity.
+     */
+    protected double value;
+
     /**
      * Gets the value in the specified quantity.
+     *
      * @return the value in the specified quantity.
      */
-    public double getValue();
-    
+    public double getValue() {
+        return value;
+    }
+
+    /**
+     * Sets the value in the specified quantity.
+     * @param value the value to set specified quantity into.
+     */
+    public void setValue(double value) {
+        this.value = value;
+    }
+
     /**
      * Gets the quantity the value is specified in.
+     *
      * @return the quantity the value is specified in.
      */
-    public Quantity getQuantity();
-    
+    public abstract Quantity getQuantity();
+
     /**
      * Gets this ValueOfQuantity as a formated string.
+     *
      * @param precision the number decimals to create the value to.
      * @return the ValueOfQuantity as a formated string.
      */
-    public String asString(int precision);
-    
+    public String asString(int precision) {
+        //Call the formator to format this value.
+        return ValueOfQuantityFormatter.formatValueOfQuantity(this, precision);
+    }
+
     /**
      * Gets this ValueOfQuantity as a formated string using default precision.
-     * default precision is defined in the implementation. 
+     * default precision is defined in the implementation.
+     *
      * @return the ValueOfQuantity as a formated string.
      */
-    public String asString();
-    
+    public String asString() {
+        return this.asString(2);
+    }
 }
